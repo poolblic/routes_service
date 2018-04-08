6b32769f74f704047c8af113ca3413ad2a2c2f67
Gustavo Aguiar <gugahoa@gmail.com>
2018-04-08 00:54:15 +0000

    Add routes app

--- routes/urls.py
+++ routes/urls.py
@@ -14,9 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from routes.view import RoutesView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('route/', include('routes.urls'))
+    path('', RoutesView.as_view(), name='routes')
 ]
