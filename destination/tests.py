6c30ae8c5fdd8ebf86deae3f83070c355ed13362
NilFurquim <nilfurquimds@gmail.com>
2018-04-08 04:51:53 -0300

    Request Google Routes

--- destination/tests.py
+++ destination/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from destination.views import DestinationView
+
 
 # Create your tests here.
+class DestinationTest(TestCase):
+    def test_get_route(self):
+        dest = DestinationView()
+        print(dest.get_route((-22.893528, -47.040663),(-22.893742, -47.040653),(-22.894152, -47.051060),(-22.909315, -47.086260)))
+        
+        return True
